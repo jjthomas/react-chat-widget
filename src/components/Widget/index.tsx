ae95105c8e5246986a829eb7d994690ab814b172
James Thomas <jamesjoethomas@gmail.com>
2023-04-19 17:15:17 -0700

    moved chat server comm here

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,10 +1,93 @@
 import { useDispatch } from 'react-redux';
+import { useState, useEffect, useCallback } from "react";
 
-import { toggleChat, addUserMessage } from '../../store/actions';
+import { toggleChat, addUserMessage, addResponseMessage } from '../../store/actions';
 import { isWidgetOpened } from '../../store/dispatcher';
 import { AnyFunction } from '../../utils/types';
+import { io } from "socket.io-client";
 
 import WidgetLayout from './layout';
+
+const API_ENDPOINT = "https://api.questacloud.com:446";
+const socket = io(API_ENDPOINT);
+
+// APIs
+
+export const createChat = async () => {
+  return (await fetch(`${API_ENDPOINT}/chat-create`, { method: "POST" })).json();
+};
+
+const getTLD = (host) => {
+  var parts = host.split(".");
+  var tld = parts.pop();
+  while (parts.length > 0) {
+    var part = parts.pop();
+    tld = part + "." + tld;
+    if (part.length >= 3) {
+      break;
+    }
+  }
+  return tld;
+};
+
+export const sendChatMessage = (chatId, message) => {
+  socket.emit("chat-message", {
+    chatId,
+    message,
+    domain: getTLD(window.location.host)
+  });
+};
+
+// Hooks
+
+const DEFAULT_CHAT_HISTORY = {
+  chatId: 0,
+  chatHistory: [],
+  suggestions: [],
+};
+
+export const useChatStream = (onNewChatMessage) => {
+  const [chatHistory, setChatHistory] = useState({
+    ...DEFAULT_CHAT_HISTORY,
+  });
+
+  useEffect(() => {
+    createChat().then(setChatHistory);
+  }, []);
+
+  const onChatHistoryUpdate = useCallback(
+    (updatedChatHistory) => {
+      if (updatedChatHistory.chatId === chatHistory.chatId) {
+        setChatHistory(updatedChatHistory);
+        onNewChatMessage(updatedChatHistory.chatHistory.slice(-1)[0], updatedChatHistory.chatHistory.length);
+      }
+    },
+    [chatHistory?.chatId, setChatHistory]
+  );
+
+  useEffect(() => {
+    socket.on("chat-history-update", onChatHistoryUpdate);
+    return () => {
+      socket.off("chat-history-update");
+    };
+  }, [onChatHistoryUpdate]);
+
+  return {
+    chatHistory,
+    sendChatMessage: (message) =>
+      sendChatMessage(chatHistory.chatId, message),
+  };
+};
+
+const handleNewServerMessage = (newMessage, id) => {
+  if (newMessage.user === 'human') {
+    // Don't render user messages.
+    return;
+  }
+  addResponseMessage(newMessage.message, id);
+};
+
+const {sendChatMessage: sendMsg} = useChatStream(handleNewServerMessage);
 
 type Props = {
   title: string;
@@ -79,7 +162,7 @@ function Widget({
 
     handleSubmit?.(userInput);
     dispatch(addUserMessage(userInput));
-    handleNewUserMessage(userInput);
+    sendMsg(userInput);
   }
 
   const onQuickButtonClicked = (event, value) => {
